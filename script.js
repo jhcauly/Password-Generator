dd1987b8c8a4f91b2281bd592ddb6d11c633de8a
jhcauly <98108408+jhcauly@users.noreply.github.com>
2022-07-23 18:42:10 -0400

    First Commit
    Here i push all files to GitHub, we can see the index, scrip and style! I just did the first function to insert Size of Password!

---
+++ script.js
@@ -0,0 +1,6 @@
+/* Here we can see the first step of the code that will include all informations of password!*/
+function firstQuestion() {
+
+    let size = prompt("You can choose from 8 to 128 characters");
+    validNumber(size);
+}
